53a2241b10d8f71132661c6a2f76fede6693ee5b
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-07-14 17:11:53 -0300

    chore: add admin.register on AlunoAdmin

--- alunos/admin.py
+++ alunos/admin.py
@@ -4,10 +4,8 @@ from alunos.models import Aluno
 
 
 # Register your models here.
+@admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'curso', 'data_nascimento', 'data_ingresso',)
     # list_filter = ('nome', 'curso',)
     search_fields = ('nome', 'curso',)
-
-
-admin.site.register(Aluno, AlunoAdmin)
