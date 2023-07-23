ac3e96aff912dd12dbc90e8bb843a43330edd368
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-07-23 16:12:22 -0300

    refact: altera campo de foto para receber link

--- alunos/models.py
+++ alunos/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-from stdimage.models import StdImageField
+# from stdimage.models import StdImageField
 
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
-    foto_perfil = StdImageField('Foto do Aluno', upload_to='alunos',
-                                variations={'thumb': (200, 200)})
+    # foto_perfil = StdImageField('Foto do Aluno', upload_to='alunos',
+    #                             variations={'thumb': (200, 200)})
+    foto_perfil = models.CharField(max_length=512)
     cpf = models.CharField(max_length=11)  # Desconsidera pontuacao
     rg = models.CharField(max_length=10)   # Desconsidera pontuacao
     curso = models.CharField(max_length=100)
